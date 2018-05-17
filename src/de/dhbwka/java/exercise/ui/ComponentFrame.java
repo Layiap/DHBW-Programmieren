c04dcb2fafa82532164458ca2462582114e54058
Matze <matthias96.haefele@live.de>
2018-05-17 12:00:23 +0200

    Initial Commit

--- src/de/dhbwka/java/exercise/ui/ComponentFrame.java
+++ src/de/dhbwka/java/exercise/ui/ComponentFrame.java
@@ -1,18 +1,26 @@
 package de.dhbwka.java.exercise.ui;
 
-import javafx.scene.control.RadioButton;
-
 import javax.swing.*;
 import java.awt.*;
+
+/*
+ * Grafische Benutzeroberflächen (GUI/Swing) (1)
+ *
+ * #######################
+ * #                     #
+ * #    Matthias Häfele  #
+ * #     2018.05.17      #
+ * #                     #
+ * #######################
+ */
 
 public class ComponentFrame {
 
     JFrame j;
 
     public ComponentFrame() {
-
+        
         j = new JFrame("Serveral basic Swing components");
-        //j.setSize(300, 300);
         j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel jp = new JPanel();
@@ -35,17 +43,23 @@ public class ComponentFrame {
         JRadioButton r2 = new JRadioButton("Hallo2");
         JRadioButton r3 = new JRadioButton("Hallo3");
 
+        // Buttons zum Panel hinzufügen
         jp2.add(r1);
         jp2.add(r2);
         jp2.add(r3);
 
+        // radio Buttons gruppieren
+        ButtonGroup group = new ButtonGroup();
+        group.add(r1);
+        group.add(r2);
+        group.add(r3);
+
+        // Beide Layouts hinzufügen
         j.add(jp, BorderLayout.NORTH);
         j.add(jp2, BorderLayout.SOUTH);
 
-        j.pack();
+        j.pack(); // Automatische Größe des Fensters
         j.setVisible(true);
-
-
     }
 
     public static void main(String[] args) {
