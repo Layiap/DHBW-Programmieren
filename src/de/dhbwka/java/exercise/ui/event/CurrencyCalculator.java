ea4c5e3de55f7f2ed22cb9cc6c152447016aa233
Matze <matthias96.haefele@live.de>
2018-05-24 12:16:27 +0200

    Changed JRE to Java 1.8

---
+++ src/de/dhbwka/java/exercise/ui/event/CurrencyCalculator.java
@@ -0,0 +1,16 @@
+package de.dhbwka.java.exercise.ui.event;
+
+/*
+ * Währungsumrechner (2)
+ *
+ * #######################
+ * #                     #
+ * #    Matthias Häfele  #
+ * #     2018.05.17      #
+ * #                     #
+ * #######################
+ */
+
+public class CurrencyCalculator {
+
+}
